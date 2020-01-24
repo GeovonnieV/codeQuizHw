beabf8f00406798c5250a5c355890936294f3122
geovonnie vasquez <geovonnievasquez@gmail.com>
2020-01-23 21:35:11 -0600

    this is my quiz

---
+++ questons.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<js>", "<javascript>", "<scripting>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        title: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["onmouseclick", "onmouseover", "onchange", "onclick"],
+        answer: "onclick"
+    },
+    {
+        title: "How do you declare a Javascript variable?",
+        choices: ["variable carName", "var carName", "v carName", "const carname"],
+        answer: "var carName"
+    },
+  ];
+
+  // 5 questions because gitlab said shoukd be 15 seconds per questions
---
+++ script.js
@@ -0,0 +1,55 @@
+var start = document.getElementById('thisBtn')
+var questionCont = document.getElementById('question1')
+var timeEl = document.querySelector(".time");
+var main = document.getElementById('mainBtn')
+//var start = document.getElementById("startBtn")
+
+var secondsLeft = 75;
+
+//function for the countdown timer 
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left in quiz";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+      //trying to make the 1st question hide in the 1st div
+      
+      
+    }
+
+  }, 1000);
+
+  
+}
+
+
+
+
+//this hides the 1st div and diplays the second div that contains the questions, it also starats the time. "method found on w3 schools"
+function startQuiz() {
+  var x = document.getElementById("beg");
+  var y = document.getElementById("question1")
+  if (x.style.display == "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+  } 
+  else {
+      x.style.display = "none";
+      y.style.display = "block";
+      setTime();
+  }
+
+  
+}
+
+
+
+
+//start.addEventListener("click", setTime)
+
+
+//how to get the questions to hide in the homepage, how to loop through the questions, how to stop the time and save time left, 
+
